8ddd82ca6f18f6998e69a17d5fde460c5dd55aed
Vitor Guedes <vittorjobs@gmail.com>
2023-11-12 15:32:39 -0300

    chore: add teste

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import express from 'express'
+import express from "express"
 // const util = require('util');
 
 const PORT = 3000
-const HOST = '0.0.0.0'
+const HOST = "0.0.0.0"
 
 const app = express()
 const teste = "João"
+console.log(teste)
 
-
-app.get('/', (req, res) => {
-    res.send('Página Inicial')
-    // util.log('INFO', 'Acessou a página inicial.');
+app.get("/", (req, res) => {
+  res.send("Página Inicial")
+  // util.log('INFO', 'Acessou a página inicial.');
 })
 
-app.get('/teste', (req, res) => {
-    res.send('Página Teste')
-    console.warn('INFO', 'Acessou a página de teste');
+app.get("/teste", (req, res) => {
+  res.send("Página Teste")
+  console.warn("INFO", "Acessou a página de teste")
 })
 
 app.listen(PORT, HOST)
